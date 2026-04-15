1061c2de77668f3f53e6e74c692c9023af10c7bd
DanielAntonio363 <0001138398@senaimgaluno.com.br>
2026-04-15 14:42:22 -0300

    Add files via upload

---
+++ Sistema.py
@@ -0,0 +1,11 @@
+numero1 = int(input("Digite um número: "))
+numero2 = input("Digite outro número: ")
+
+if numero1 > numero2:
+    print("O primeiro número é maior que o segundo.")
+
+elif numero1 < numero2:
+print("O segundo número é maior que o primeiro.")
+
+else:
+    print("Os dois números são iguais.")
